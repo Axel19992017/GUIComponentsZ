bbe2f0e63e8d637f4cf77da66e1711db5980086a
admin <admin@AxelPC>
2019-12-19 22:23:24 -0600

    añadiendo retoques

--- src/swing/Controles/LabelZ.java
+++ src/swing/Controles/LabelZ.java
@@ -31,7 +31,7 @@ public class LabelZ extends JLabel {
 
         setBackground(new java.awt.Color(255, 247, 162));
         setFont(new java.awt.Font("Roboto", 1, 12)); // NOI18N
-        setForeground(java.awt.Color.gray);
+        setForeground(new java.awt.Color(51, 51, 51));
         setText("SAMPLE");
         setOpaque(true);
     }// </editor-fold>//GEN-END:initComponents
